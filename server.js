528eb3bfc0e98a1fe8e4fe83478bfac9f6671d1c
Duc Nguyen <ducntq@gmail.com>
2013-08-12 14:17:40 +0700

    Thêm serve từ statics

--- server.js
+++ server.js
@@ -3,35 +3,68 @@ var app = express();
 var url = require('url');
 var redis = require("redis");
 var crypto = require('crypto');
+var fs = require('fs');
 app.use(express.bodyParser()); // init middle ware for post
 
+var staticsPath = './statics';
+
+
+// create md5 hash method
 var md5 = function(input) {
     return crypto.createHash('md5').update(input).digest("hex");
+};
+
+// inject endsWith method to String object
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+String.prototype.endsWithOneOf = function (suffixArray) {
+    for (var i = 0, total = suffixArray.length; i < total; i++) {
+        if (this.endsWith(suffixArray[i])) return true;
+    }
+    return false;
 };
 
 app.use(function(req, res){
     var absoluteUrl = req.protocol + "://" + req.host + req.url; // have no port
     var key = md5(absoluteUrl);
-    if(req.method == 'GET') {
-        var client = redis.createClient(6379, '127.0.0.1');
-        client.get(key, function (err, reply) {
-            console.log('GET Reply:');
-            console.log(reply);
-            if (reply) res.send(reply);
+    var client = redis.createClient(6379, '127.0.0.1');
+
+    var staticsSuffix = ['.js', '.css', '.jpg'];
+
+    if (absoluteUrl.endsWithOneOf(staticsSuffix)) {
+        var file = staticsPath + req.url;
+        console.log('GET STATICS:');
+        console.log(file);
+
+        fs.exists(file, function(exists) {
+            if (exists) {
+                res.sendfile(file);
+            }
             else res.send(404);
         });
     }
-    else if (req.method == 'POST' && req.body.content) {
-        var client = redis.createClient(6379, '127.0.0.1');
-        client.set(key, req.body.content, function(err, reply){
-            console.log('POST Reply:');
-            console.log(reply);
-            res.send(reply);
-        });
-    }
     else {
-        console.log('Unhandled method: ' + req.method);
-        res.send(404);
+        if(req.method == 'GET') {
+            client.get(key, function (err, reply) {
+                console.log('GET Reply:');
+                console.log(reply);
+                if (reply) res.send(reply);
+                else res.send(404);
+            });
+        }
+        else if (req.method == 'POST' && req.body.content) {
+            client.set(key, req.body.content, function(err, reply){
+                console.log('POST Reply:');
+                console.log(reply);
+                res.send(reply);
+            });
+        }
+        else {
+            console.log('Unhandled method: ' + req.method);
+            res.send(404);
+        }
     }
 });
 
